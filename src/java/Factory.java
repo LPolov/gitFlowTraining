7ff852b3e43be52ef48ea32f5b56f8d9593190ef
Leontii Polovinko <leontii_polovinko@epam.com>
2021-08-04 11:36:53 +0300

    my commit with diff

--- src/java/Factory.java
+++ src/java/Factory.java
@@ -1,7 +1,10 @@
 package java;
 
 public class Factory {
+  public Factory() {
+    System.out.println("Factory was created");
+  }
   public static void main(String[] args) {
-    System.out.println("Heeey");
+    System.out.println("");
   }
 }
