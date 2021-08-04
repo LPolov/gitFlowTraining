44e1c9bdd1af32a365caf054b2b6a419eb2d7fe8
Leontii Polovinko <leontii_polovinko@epam.com>
2021-08-04 14:55:57 +0300

    to check sha

--- src/java/Employee.java
+++ src/java/Employee.java
@@ -7,6 +7,6 @@ public class Employee {
   }
 
   public static void main(String[] args) {
-    System.out.println();
+    System.out.println("Hello there");
   }
 }
--- src/java/Factory.java
+++ src/java/Factory.java
@@ -5,6 +5,6 @@ public class Factory {
     System.out.println("Factory was created");
   }
   public static void main(String[] args) {
-    System.out.println("");
+    System.out.println("Hello there");
   }
 }
