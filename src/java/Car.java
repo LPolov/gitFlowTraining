e4d1d984636ada456e5a58b3d254acfb94ec9ecf
Leontii Polovinko <leontii_polovinko@epam.com>
2021-08-09 15:43:37 +0300

    add arg to Car meth

--- src/java/Car.java
+++ src/java/Car.java
@@ -4,7 +4,7 @@ public class Car {
   public static void main(String[] args) {
 
   }
-  public void ride() {
+  public void ride(String name) {
     System.out.println("");
   }
 }
--- src/java/Wheal.java
+++ src/java/Wheal.java
@@ -2,5 +2,11 @@ package java;
 
 public class Wheal {
   public Wheal() {
+    System.out.println("At all don't need it");
+  }
+
+  @Override
+  public String toString() {
+    return "Wheal{}";
   }
 }
