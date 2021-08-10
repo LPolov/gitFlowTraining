e0b0050c18503a652be5e85d1e9620a816d6ae6d
Leontii Polovinko <leontii_polovinko@epam.com>
2021-08-10 17:51:28 +0300

    add extra sout in ride()

--- src/java/Car.java
+++ src/java/Car.java
@@ -7,12 +7,15 @@ public class Car {
 
   public Car(List<Wheal> whealList) {
     this.whealList = whealList;
+    System.out.println("Constructor new");
+
   }
 
   public static void main(String[] args) {
 
   }
   public void ride(String name) {
+    System.out.println("Riding " + name);
     System.out.println("Riding " + name);
   }
 }
