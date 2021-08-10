e7d283d18da7aa27e27ba4efeaaf1adba5c3d303
Leontii Polovinko <leontii_polovinko@epam.com>
2021-08-10 15:35:04 +0300

    Change Factory

--- src/java/Factory.java
+++ src/java/Factory.java
@@ -2,9 +2,9 @@ package java;
 
 public class Factory {
   public Factory() {
-    System.out.println("Factory was created");
+    System.out.println("Factory was created!");
   }
   public static void main(String[] args) {
-    System.out.println("Hello there");
+    System.out.println("Hello there!");
   }
 }
